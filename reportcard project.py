b50da1323f5997f370013e1ac4c12000ee44b2f9
Rayne <69247113+Rayne231@users.noreply.github.com>
2023-01-13 14:52:39 +0530

    second

--- reportcard project.py
+++ reportcard project.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
+# Copyright (C) 2022 Saatvik Nagpal and Abhinav Saklani
+
+# This program is free software: under the terms of the GNU General 
+# Public License as published by the Free Software Foundation, 
+# either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import mysql.connector as sqlcon
 mydb=sqlcon.connect(host="localhost", user="root", password="student", database="reportcards")
 
